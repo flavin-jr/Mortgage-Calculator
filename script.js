0a2f0b92f119971ecdc7aa1817a9ec6d35017dd0
flavin-jr <fjcvj@cin.ufpe.br>
2024-08-20 15:01:13 -0300

    fix: radio inputs check and check inputs function

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ const form = document.querySelector('form')
 const mortgageAmountInput = document.querySelector('#mortgage-amount');
 const mortgageTermInput = document.querySelector('#mortgage-term');
 const interestRateInput = document.querySelector('#interest-rate');
-const mortgageTypeInputs = document.querySelectorAll('input[type = "radio"]');
+const radioInputs = document.querySelectorAll('input[type="radio"]');
 const calculateBtn = document.querySelector('.calculate-btn');
-
 const rightContent = document.querySelector('.right-content')
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const readyToSubmit = checkInputs();
+
     if (readyToSubmit) {
+        const [amount, term, rate, type] = readyToSubmit;
+        calculate(amount, term, rate, type)
         changeRightContent()
     }
-    console.log(readyToSubmit)
+    console.log('rodei')
 })
 function checkRadioInputs() {
-    const radioInputListCheckeds = [...mortgageTypeInputs].filter(radioInput => {
-        radioInput.hasAttribute('checked')
-    });
+    const mortgageTypeInputChecked = document.querySelector('input[name = "mortgage-type"]:checked');
 
-    return radioInputListCheckeds.length > 0 ? true : false;
+    return mortgageTypeInputChecked
 }
 
 
@@ -27,7 +27,7 @@ function checkInputs() {
     const mortgageAmountValue = mortgageAmountInput.value;
     const mortgageTermValue = mortgageTermInput.value;
     const interestRateValue = interestRateInput.value;
-    const mortgageTypeInputsList = [...mortgageTypeInputs]
+    const radioInputsList = [...radioInputs]
     if (mortgageAmountValue === '') {
         setErrorFor(mortgageAmountInput);
     }
@@ -37,15 +37,20 @@ function checkInputs() {
     if (interestRateValue === '') {
         setErrorFor(interestRateInput);
     }
-    if (!checkRadioInputs()) {
-        setErrorFor(mortgageTypeInputsList.pop())
+    const radioInputChecked = checkRadioInputs()
+    if (!radioInputChecked) {
+        setErrorFor(radioInputsList[1])
     }
 
-    if (mortgageAmountValue === '' && mortgageTermValue === '' && interestRateValue === '' && !checkRadioInputs()) {
+    if (mortgageAmountValue === '' && mortgageTermValue === '' && interestRateValue === '' && !radioInputChecked) {
+        return false
+    }
+    if (isNaN(mortgageAmountValue) || isNaN(mortgageTermValue) || isNaN(interestRateValue)) {
         return false
     }
 
-    return true
+    const mortgageValues = [parseFloat(mortgageAmountValue), parseInt(mortgageTermValue), parseFloat(interestRateValue), radioInputChecked.value]
+    return mortgageValues
 
 }
 
@@ -95,3 +100,23 @@ function changeRightContent() {
 
 
 }
+
+function calculate(amount, term, rate, type) {
+    console.log(amount, term, rate, type)
+    let monthlyPayment;
+    if (type === 'repayment') {
+
+        const monthlyInterestRate = rate / 12 / 100;
+        const numberOfPayments = term * 12;
+
+        monthlyPayment = (amount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
+            (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+
+    } else if (type === 'interest-only') {
+
+        monthlyPayment = amount * (rate / 100);
+    }
+
+    console.log(monthlyPayment.toFixed(2))
+}
+
